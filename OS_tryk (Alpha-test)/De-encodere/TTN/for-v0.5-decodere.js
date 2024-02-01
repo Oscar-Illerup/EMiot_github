d16a4fcfbbe3185342311031eaf27e7eca2d1562
Oscar Illerup <oillerup@gmail.com>
2024-02-01 13:25:23 +0100

    add of en/decoder for v0.5

---
+++ OS_tryk (Alpha-test)/De-encodere/TTN/for-v0.5-decodere.js
@@ -0,0 +1,34 @@
+function decodeUplink(input) {
+  var data = {};
+  if (input.fPort == 1) {
+  data.voltage = (input.bytes[0]/10);
+  data.reason = 0;
+  }
+  if (input.fPort == 2) {
+  data.reason = 1;
+  data.voltage = (input.bytes[0]/10);
+  let Mac_add = [];
+  for (let i= 1; i < input.bytes.length; i++) {
+    Mac_add[i] = input.bytes[i];
+  }
+  let mac_rssi_combi = Mac_add.map((byte) => {
+    return String.fromCharCode(byte);
+  }).join("");
+  mac_rssi_combi = mac_rssi_combi.split("|");
+  mac_rssi_combi.pop();
+  data.mac = [];
+  data.rssi = [];
+  let j = 0;
+  for (let i= 0; i < mac_rssi_combi.length; i++) {
+    data.mac[j] = mac_rssi_combi[i];
+    data.rssi[j] =  parseInt(mac_rssi_combi[i + 1]);
+    i++
+    j++
+  }
+  }
+  return {
+  data: data,
+  warnings: [],
+  errors: []
+  };
+}
---
+++ OS_tryk (Alpha-test)/De-encodere/TTN/for-v0.5-encodere.js
@@ -0,0 +1,43 @@
+var heatbeat_interval = [30,60,120,240,360,480,600,720];
+var confirmed_heatbeat = [false, true];
+var ble_scanner = ["off", "on"];
+var ble_prefix_1 = ["ac233f","oc0001","oc0002","oc0003"];
+var ble_prefix_2 = ["ac233f","oc0001","oc0002","oc0003"];
+var ble_prefix_3 = ["ac233f","oc0001","oc0002","oc0003"];
+var factory_reset = ["yes"];
+
+function encodeDownlink(input) {
+  return {
+    bytes: [
+    heatbeat_interval.indexOf(input.data.heatbeat_interval),
+    confirmed_heatbeat.indexOf(input.data.confirmed_heatbeat),
+    ble_scanner.indexOf(input.data.ble_scanner),
+    ble_prefix_1.indexOf(input.data.ble_prefix_1),
+    ble_prefix_2.indexOf(input.data.ble_prefix_2),
+    ble_prefix_3.indexOf(input.data.ble_prefix_3),
+    factory_reset.indexOf(input.data.factory_reset)
+    ],
+    fPort: 10,
+  };
+}
+
+function decodeDownlink(input) {
+  switch (input.fPort) {
+  case 10:
+    return {
+      data: {
+        heatbeat_interval: heatbeat_interval[input.bytes[0]],
+        confirmed_heatbeat: confirmed_heatbeat[input.bytes[1]],
+        ble_scanner: ble_scanner[input.bytes[2]],
+        ble_prefix_1: ble_prefix_1[input.bytes[3]],
+        ble_prefix_2: ble_prefix_1[input.bytes[4]],
+        ble_prefix_3: ble_prefix_1[input.bytes[5]],
+        factory_reset: factory_reset[input.bytes[6]]
+      }
+    }
+  default:
+    return {
+      errors: ["unknown FPort"]
+    }
+  }
+}
